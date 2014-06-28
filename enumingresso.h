ad194a1703d539dbd7ad61d9e10fe74a6b78c1f0
GustavoMunhoz <gustavo_munhoz@outlook.com>
2014-06-28 16:57:32 -0300

    EnumSeparado

---
+++ enumingresso.h
@@ -0,0 +1,30 @@
+#ifndef ENUM_H
+#define ENUM_H
+
+
+class Enum
+{
+    private:
+        int qtdIngressos;
+        int qtdMeia;
+        int preco;
+    public:
+        enum ingressos
+        {
+            MEIA,
+            INTEIRA
+        };
+
+        void ingressos(int x){
+            if(x == INTEIRA)qtdIngressos++;
+            if(x == MEIA)qtdMeia++;
+        }
+
+        void inicPreco(int x,float precInt){
+            float precoInt =precInt,precoMeia=(precInt)/2;
+            if(x == INTEIRA)preco = precoMeia;
+            if(x == MEIA)preco = precoInt;
+        }
+};
+
+#endif // ENUM_H
---
+++ enumpag.h
@@ -0,0 +1,5 @@
+enum formaPagMentO
+        {
+            DINHEIRO,
+            CARTAO
+        };
